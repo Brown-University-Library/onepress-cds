8c2c7edcc655f877a2aafd2e4350797fd3f97296
Patrick Rashleigh <edward_rashleigh@brown.edu>
2017-03-24 11:59:00 -0400

    Changes made by Crystal/Brian/Patrick on WorfDev

---
+++ single.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * The template for displaying all single posts.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package OnePress
+ */
+
+get_header();
+/* $layout = get_theme_mod( 'onepress_layout', 'right-sidebar' ); */
+$layout = get_theme_mod( 'onepress_layout' );
+?>
+
+	<div id="content" class="site-content">
+
+		<?php echo onepress_breadcrumb(); ?>
+             
+		<div id="content-inside" class="container <?php echo esc_attr( $layout ); ?>">
+			<div id="primary" class="content-area">
+              
+                <div id="site_content_menu"> 
+                    <ul>
+                        <?php
+                            wp_nav_menu(array('theme_location' => 'primary', 'container' => '', 'items_wrap' => '%3$s'));  
+                        ?>
+                    </ul>
+                </div>
+              
+              
+				<main id="main" class="site-main" role="main">
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'template-parts/content', 'single' ); ?>
+
+					<?php
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+					?>
+
+				<?php endwhile; // End of the loop. ?>
+
+				</main><!-- #main -->
+			</div><!-- #primary -->
+
+            <?php if ( false || $layout != 'no-sidebar' ) { ?>
+                <?php get_sidebar(); ?>
+            <?php } ?>
+
+		</div><!--#content-inside -->
+	</div><!-- #content -->
+
+<?php get_footer(); ?>
---
+++ template-parts/content-single.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Template part for displaying single posts.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package OnePress
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'onepress' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php onepress_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
+
